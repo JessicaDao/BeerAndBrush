475ec830c69debb82cc94552a48db3ad3b39f8ec
Tyler <thibault.tyler@gmail.com>
2021-02-24 10:26:45 -0800

    change server.js file

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var express = require("express");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -23,8 +23,8 @@ app.use(express.static("public"));
 
 // Routes
 // =============================================================
-require("./routes/html-routes.js")(app);
-require("./routes/router-user-api.js")(app);
+require("./routes/api/html.js")(app);
+require("./routes/api/user.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
