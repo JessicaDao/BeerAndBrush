c05365217c0d3ba9f4b916d80ae09897df68c8a5
Tyler <thibault.tyler@gmail.com>
2021-02-25 12:03:34 -0800

    updated server.js file

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,4 @@
 module.exports = app => {
-    app.use('api/user', require('./userController'))
+    app.use('api/user', require('./user-controller'))
     app.use('api/project', require('./project-controller'))
 }
