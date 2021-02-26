8fb13d0fcc54db8a4b90340368ff8ca3252780b8
Jessica Dao <xjessicadao@gmail.com>
2021-02-25 23:31:45 -0800

    review model created, control wip

--- controllers/review-controller.js
+++ controllers/review-controller.js
@@ -7,8 +7,21 @@ const { jsxText } = require("@babel/types");
 
 
 // ***************************************** C ****
-router.post("/new", (req, res) => {
-    // db.
+router.post("/",(req,res)=>{
+    if(!req.session.user){
+        res.status(401).send("Please login.")
+    } else {
+    db.reviews.create({
+        class: req.body.name,
+        reviewer: req.body.level,
+        content: req.body.date,
+        UserId: req.session.user.id
+        }).then(data=>{
+            res.json(data);
+            }).catch(err=>{
+                res.status(500).json(err);
+            })
+    }
 })
 
 // ***************************************** R ****
