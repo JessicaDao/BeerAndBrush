aa5783f8bde2191a96058e41a76c19ab2d9b8df4
Tyler <thibault.tyler@gmail.com>
2021-02-26 09:10:23 -0800

    added models folder

---
+++ models/gallery-model.js
@@ -0,0 +1,27 @@
+module.exports = function (sequelize, DataTypes) {
+    var Gallery = sequelize.define('Gallery', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        bio: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        materialUsed: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        forSale: {
+            type: DataTypes.BOOLEAN,
+            default: false
+        }
+    });
+    { timestamps: true }
+
+    return Gallery;
+}
