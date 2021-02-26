290915c45ccd70d9850c7a3a9545bc95236760f6
Jessica Dao <xjessicadao@gmail.com>
2021-02-25 23:07:54 -0800

    class create and db created

--- controllers/class-controller.js
+++ controllers/class-controller.js
@@ -4,17 +4,40 @@ const db = require("../models");
 const user = require("../models/user");
 const bcrypt = require("bcrypt");
 const { jsxText } = require("@babel/types");
+const classDetails = require("../models/class-details");
 
 
+router.get("/", (req, res)=>{
+    db.classDetails.findAll().then(data=>{
+        res.json(data)
+    }).catch(err=>{
+        res.status(500).json(err);
+    })
+})
 // ***************************************** C ****
-router.post("/new", (req, res) => {
-    // db.
+router.post("/", (req, res)=>{
+    if(!req.session.user){
+        res.status(401).send("Please login.")
+    }else {
+    db.classDetails.create({
+        name: req.body.name,
+        level: req.body.level,
+        date: req.body.date,
+        time: req.body.time,
+        duration: req.body.duration,
+        price: req.body.price,
+        location: req.body.location,
+        price: req.body.price,
+        reviews: req.body.reviews,
+        UserId: req.session.user.id
+        }).then(data=>{
+            res.json(data);
+            }).catch(err=>{
+                res.status(500).json(err);
+            })
+    }
 })
 
-// ***************************************** R ****
 
-// ***************************************** U ****
-
-// ***************************************** D ****
 
 module.exports = router;
