a3a4912a3039661864ac10593d25bb3408b7c59f
Jessica Dao <xjessicadao@gmail.com>
2021-02-28 16:11:35 -0800

    portfolio updated

--- controllers/portfolio-controller.js
+++ controllers/portfolio-controller.js
@@ -28,15 +28,95 @@ const authenticateMe = (req) => {
     }
     return data;
 }
+
+router.get("/", (req, res) => {
+    db.Portfolio.findAll()
+      .then((portfolios) => {
+        res.json(portfolios);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 // ***************************************** C ****
-router.post("/new", (req, res) => {
-    // db.
+
+  router.post("/", (req, res) => {
+    const userData = authenticateMe(req);
+    if (!userData) {
+        res.status(403).send("Please log in.");
+    } else {
+        db.Portfolio.create({
+            name: req.body.name,
+            UserId: userData.id
+        }).then(newPortfolio => {
+            res.json(newPortfolio)
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    }
 })
 
 // ***************************************** R ****
+router.get("/portfolios", (req, res) => {
+    db.Portfolio.findAll({
+        include: [db.Gallery]
+    }).then(portfolios => {
+        res.json(portfolios)
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
 
 // ***************************************** U ****
+router.get("/:id/portfolios", (req, res) => {
+    const userData = authenticateMe(req);
+    
+    db.Portfolio.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [db.Gallery]
+    }).then(portfolio => {
+        console.log(userData)
+    console.log(portfolio);
+        res.json({ portfolio: portfolio, canEdit: (userData && userData.id === portfolio.UserId) })
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
 
 // ***************************************** D ****
+router.delete("/:id",(req,res)=>{
+    const userData = authenticateMe(req);
+    db.Portfolio.findOne({
+        where:{
+            id:req.params.id
+        }
+    }).then(portfolio=>{
+        if(portfolio.UserId===userData.id){
+            db.Portfolio.destroy({
+                where:{
+                    id:req.params.id
+                }
+            }).then(delPortfolio=>{
+                res.json(delPortfolio)
+            }).catch(err=>{
+                console.log(err)
+                res.status(500).json(err)
+            })
+        }else {
+            res.status(403).send("Wrong portfolio.")
+        }
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json(err)
+    })
+})
+
 
 module.exports = router;
