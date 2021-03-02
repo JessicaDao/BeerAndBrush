cc184c190a008dbe60903f03c41a40c907795057
Jessica Dao <xjessicadao@gmail.com>
2021-03-01 20:19:02 -0800

    few controller mods

--- controllers/project-controller.js
+++ controllers/project-controller.js
@@ -5,137 +5,137 @@ const bcrypt = require("bcrypt");
 const { jsxText } = require("@babel/types");
 
 const authenticateMe = (req) => {
-    let token = false;
-    if (!req.headers) {
-        token = false
-    }
-    else if (!req.headers.authorization) {
-        token = false;
-    }
-    else {
-        token = req.headers.authorization.split(" ")[1];
-    }
-    let data = false;
-    if (token) {
-        data = jwt.verify(token, "bananas", (err, data) => {
-            if (err) {
-                return false;
-            } else {
-                return data
-            }
-        })
-    }
-    return data;
-}
+  let token = false;
+  if (!req.headers) {
+    token = false;
+  } else if (!req.headers.authorization) {
+    token = false;
+  } else {
+    token = req.headers.authorization.split(" ")[1];
+  }
+  let data = false;
+  if (token) {
+    data = jwt.verify(token, "bananas", (err, data) => {
+      if (err) {
+        return false;
+      } else {
+        return data;
+      }
+    });
+  }
+  return data;
+};
 
 // ***************************************** C ****
 //working
-router.post("/new", (req, res) => {
+router.post("/newProject", (req, res) => {
+  const userData = authenticateMe(req);
+  if (!userData) {
+    res.status(403).send("Login first.");
+  } else {
     db.Project.create({
-        title: req.body.title,
-        artistName: req.body.artistName,
-        dateStarted: req.body.dateStarted,
-        dateFinished: req.body.dateFinished,
-        description: req.body.description,
-        materialUsed: req.body.materialUsed,
-        category: req.body.category,
-        forSale: req.body.forSale
-    }).then(resp => {
-        res.json({
-            data: resp
-        })
-    }).catch(err => {
-        res.status(500).json(err);
+      title: req.body.title,
+      artistName: req.body.artistName,
+      dateStarted: req.body.dateStarted,
+      dateFinished: req.body.dateFinished,
+      description: req.body.description,
+      materialUsed: req.body.materialUsed,
+      category: req.body.category,
+      forSale: req.body.forSale,
     })
-})
-
+      .then((newProject) => {
+        res.json(newProject);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  }
+});
 
 // ***************************************** R ****
 // Route to show public gallery items
-
+// Keep this above findOne
 router.get("/gallery", async (req, res) => {
-    let findProject = await db.Project.findAll(
-        {
-            where: {
-                isPublic: true
-            }
-        })
-    res.json({
-        data: findProject,
-        msg: "Gallery found."
-    })
-})
+  let findProject = await db.Project.findAll({
+    where: {
+      isPublic: true,
+    },
+  });
+  res.json({
+    data: findProject,
+    msg: "Gallery found.",
+  });
+});
 
 router.get("/:project_id", async (req, res) => {
-    let findProject = await db.Project.findOne(
-        {
-            where: {
-                id: req.params.project
-            }
-        })
-    res.json({
-        data: findProject,
-        msg: "Project found."
-    })
-})
-
+  let findProject = await db.Project.findOne({
+    where: {
+      id: req.params.project,
+    },
+  });
+  res.json({
+    data: findProject,
+    msg: "Project found.",
+  });
+});
 
 // Do we need a findAll option? Would that be specific to user id as well?
 
 // ***************************************** U ****
 //working
 router.put("/update/:project_id", (req, res) => {
-    db.Project.update(req.body, {
-        where: {
-            id: req.params.project_id
-        }
-    }).then(resp => {
-        res.json({
-            data: resp,
-            msg: "Project updated."
-        })
-    }).catch(err => {
-        res.status(500).json(err);
+  db.Project.update(req.body, {
+    where: {
+      id: req.params.project_id,
+    },
+  })
+    .then((resp) => {
+      res.json({
+        data: resp,
+        msg: "Project updated.",
+      });
     })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 
-
-    // res.json({
-    //     data: projectUpdate,
-    //     msg: "Project updated."
-    // })
-})
+  // res.json({
+  //     data: projectUpdate,
+  //     msg: "Project updated."
+  // })
+});
 // ***************************************** D ****
 
 //Project delete
 router.delete("/delete/:project_id", (req, res) => {
-    const userData = authenticateMe(req);
-    db.Project.findOne({
-      where: {
-        id: req.params.id,
-      },
-    })
-      .then((project) => {
-        if (project.UserId === userData.id) {
-          db.Project.destroy({
-            where: {
-              id: req.params.id,
-            },
+  const userData = authenticateMe(req);
+  db.Project.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((project) => {
+      if (project.UserId === userData.id) {
+        db.Project.destroy({
+          where: {
+            id: req.params.id,
+          },
+        })
+          .then((delProject) => {
+            res.json(delProject);
           })
-            .then((delProject) => {
-              res.json(delProject);
-            })
-            .catch((err) => {
-              console.log(err);
-              res.status(500).json(err);
-            });
-        } else {
-          res.status(403).send("Try again.");
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      } else {
+        res.status(403).send("Try again.");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
