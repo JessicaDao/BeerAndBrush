50b23d3c84d2e1b18685d314fe8c192de51163e9
Tyler <thibault.tyler@gmail.com>
2021-03-03 10:54:33 -0800

    random push

--- controllers/class-controller.js
+++ controllers/class-controller.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require("../models");
 const user = require("../models/user-model");
 const bcrypt = require("bcrypt");
-const { jsxText } = require("@babel/types");
+
 
 const authenticateMe = (req) => {
     let token = false;
@@ -57,6 +57,7 @@ router.post("/new", (req, res) => {
     }
 })
 
+// previous code
 // router.post("/", (req, res) => {
 //     const userData = authenticateMe(req);
 //     if (!userData) {
@@ -74,7 +75,6 @@ router.post("/new", (req, res) => {
 //                     date: req.body.date,
 //                     time: req.body.time,
 //                     duration: req.body.duration,
-//                     price: req.body.price,
 //                     location: req.body.location,
 //                     price: req.body.price,
 //                     UserId: userData.id
@@ -96,6 +96,7 @@ router.post("/new", (req, res) => {
 
 
 // ***************************************** R ****
+// Finds all classes
 router.get("/all", (req, res) => {
     db.Class.findAll().then(data => {
         res.json(data)
@@ -104,6 +105,7 @@ router.get("/all", (req, res) => {
     })
 })
 
+// Finds specific instructor classes
 router.get('/:id', (req, res) => {
     db.Class.findAll({
         where: {
@@ -120,46 +122,46 @@ router.get('/:id', (req, res) => {
 })
 
 // ***************************************** U ****
-router.put("/classes/update/:id", (req, res) => {
-    if (!req.session.user) {
-        res.status(401).send("Unable to retrieve.")
-    } else {
-        let classObj = {};
-        if (req.body.name !== null && req.body.name !== "") {
-            classObj.name = req.body.name;
-        }
-        if (req.body.level !== null && req.body.level !== "") {
-            classObj.level = req.body.level;
-        }
-        if (req.body.date !== null && req.body.date !== "") {
-            classObj.date = req.body.date;
-        }
-        if (req.body.time !== null && req.body.time !== "") {
-            classObj.time = req.body.time;
-        }
-        if (req.body.duration !== null && req.body.duration !== "") {
-            classObj.duration = req.body.duration;
-        }
-        if (req.body.recurring !== null && req.body.recurring !== "") {
-            classObj.recurring = req.body.recurring;
-        }
-        if (req.body.price !== null && req.body.price !== "") {
-            classObj.price = req.body.price;
-        }
-        if (req.body.location !== null && req.body.location !== "") {
-            classObj.location = req.body.location;
-        }
-        db.classDetails.update(classObj, {
-            where: {
-                userId: req.session.user.id,
-                id: req.params.id
-            }
-        }).then(data => {
-            res.json(data);
-        }).catch(err => { res.status(500).send(err.message); });
+// router.put("/classes/update/:id", (req, res) => {
+//     if (!req.session.user) {
+//         res.status(401).send("Unable to retrieve.")
+//     } else {
+//         let classObj = {};
+//         if (req.body.name !== null && req.body.name !== "") {
+//             classObj.name = req.body.name;
+//         }
+//         if (req.body.level !== null && req.body.level !== "") {
+//             classObj.level = req.body.level;
+//         }
+//         if (req.body.date !== null && req.body.date !== "") {
+//             classObj.date = req.body.date;
+//         }
+//         if (req.body.time !== null && req.body.time !== "") {
+//             classObj.time = req.body.time;
+//         }
+//         if (req.body.duration !== null && req.body.duration !== "") {
+//             classObj.duration = req.body.duration;
+//         }
+//         if (req.body.recurring !== null && req.body.recurring !== "") {
+//             classObj.recurring = req.body.recurring;
+//         }
+//         if (req.body.location !== null && req.body.location !== "") {
+//             classObj.location = req.body.location;
+//         }
+//         if (req.body.price !== null && req.body.price !== "") {
+//             classObj.price = req.body.price;
+//         }
+//         db.classDetails.update(classObj, {
+//             where: {
+//                 userId: req.session.user.id,
+//                 id: req.params.id
+//             }
+//         }).then(data => {
+//             res.json(data);
+//         }).catch(err => { res.status(500).send(err.message); });
 
-    }
-});
+//     }
+// });
 
 
 
